65d9a8f7d4ca6c119236acd61de745ad54458667
Anam Navied <anavied@ncsu.edu>
2020-02-19 00:37:24 -0500

    created data structures to store stats data, and sent dummy data to zpage

--- zpages/rpc.py
+++ zpages/rpc.py
@@ -1,31 +1,70 @@
 from flask import Flask, escape, request, render_template
+from typing import NamedTuple
+import datetime
 
 app = Flask(__name__)
 
+
+class statSnapshot(NamedTuple):
+    method: str
+    received: bool
+    countMinute: int
+    countHour: int
+    countTotal: int
+    avgLatencyMinute: datetime.datetime
+    avgLatencyHour: datetime.datetime
+    avgLatencyTotal: datetime.datetime
+    rpcRateMinute: float
+    rpcRateHour: float
+    rpcRateTotal: float
+    inputRateMinute: float
+    inputRateHour: float
+    inputRateTotal: float
+    outputRateMinute: float
+    outputRateHour: float
+    outputRateTotal: float
+    errorsMinute: int
+    errorsHour: int
+    errorsTotal: int
+
+
+class statGroup(NamedTuple):
+    direction: str
+    snapshots: list # of type statSnapshot, can't specify explicitly but will only consist of this
+
+
+class statPage(NamedTuple):
+    statgroups: list # of type statGroup, can't specify explicitly but will only consist of this
+
+
 @app.route('/')
-def hello():
-    #name = request.args.get("name", "World")
-    direction = "Sent"
-    template_values = {'Direction': direction}
+def showPage():
+    directionDummy = "Sent"
+    test_row = statSnapshot('apicall1()', False, 1, 2, 3, '2020-02-18 23:46:31.243168', '2020-02-18 23:46:31.243168', '2020-02-18 23:46:31.243168', 1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3, 4, 5, 6)
+    # template_values = {'Direction': directionDummy}
     # path = os.path.join(os.path.dirname(__file__), 'index.html')
     # self.response.out.write(template.render(path, template_values))
 
-    return render_template("index.html", Direction = direction)
+    return render_template("index.html", Direction=directionDummy,
+                           method=test_row.method,
+                           countMinute=test_row.countMinute,
+                           countHour=test_row.countHour,
+                           countTotal=test_row.countTotal,
+                           avgLatencyMinute=test_row.avgLatencyMinute,
+                           avgLatencyHour=test_row.avgLatencyHour,
+                           avgLatencyTotal=test_row.avgLatencyTotal,
+                           rpcRateMinute=test_row.rpcRateMinute,
+                           rpcRateHour=test_row.rpcRateHour,
+                           rpcRateTotal=test_row.rpcRateTotal,
+                           inputRateMinute=test_row.inputRateMinute,
+                           inputRateHour=test_row.inputRateHour,
+                           inputRateTotal=test_row.inputRateTotal,
+                           outputRateMinute=test_row.outputRateMinute,
+                           outputRateHour=test_row.outputRateHour,
+                           outputRateTotal=test_row.outputRateTotal,
+                           errorsMinute=test_row.errorsMinute,
+                           errorsHour=test_row.errorsHour,
+                           errorsTotal=test_row.errorsTotal)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# class CalcController(webapp.RequestHandler):
-#   def get(self):
-#     principalString=self.request.get('principal')
-#     rateString = self.request.get('rate')
-#     principal = int(principalString)
-#     rate = int(rateString)
-#     interest = principal*rate/100
-#     interestString = str(interest)
-#    # set up the template_values with the list of people returned.
-#     template_values= {'interest':interestString, 'principal': principalString}
-#     # render the page using the template engine
-#     path = os.path.join(os.path.dirname(__file__),'index.html')
-#     self.response.out.write(template.render(path,template_values))
