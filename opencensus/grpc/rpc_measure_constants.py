fbaa6e2b8cfa4ddee557610a7d4e42ac4fe1a1d5
Anam Navied <anavied@ncsu.edu>
2020-04-07 17:55:22 -0400

    removed deprecated measures, according to specs doc

--- opencensus/grpc/rpc_measure_constants.py
+++ opencensus/grpc/rpc_measure_constants.py
@@ -6,59 +6,106 @@ class RPCMeasureConstants():
     """
     constants for collecting RPC stats
     """
-
+    byte = "by"
+    count = "1"
+    millisecond = "ms"
 
     def __init__(self):
         """
         needs fixing.
         todo: do I put all the RPC constant fields in here?
         """
-        @deprecated(version=0.8, reason="sf")
-        rpc_status = tag_key.TagKey("canonical_status") #deprecated
 
-        rpc_method = tag_key.TagKey("method") #deprecated
+        # rpc_status = tag_key.TagKey("canonical_status") # @deprecated(version=0.8, reason="sf")
+        # rpc_method = tag_key.TagKey("method") # @deprecated(version=0.8, reason="sf")
+
+        #client tags
+        # gRPC server status code received, e.g. OK, CANCELLED, DEADLINE_EXCEEDED
         grpc_client_status = tag_key.TagKey("grpc_client_status")
-        grpc_server_status = tag_key.TagKey("grpc_server_status")
+        # Full gRPC method name, including package, service and method, e.g. google.bigtable.v2.Bigtable/CheckAndMutateRow
         grpc_client_method = tag_key.TagKey("grpc_client_method")
-        grpc_server_method = tag_key.TagKey("grpc_server_method")
-        byte = "by"
-        count = "1"
-        millisecond = "ms"
 
-        # the MeasureDouble and MeasureLong
-        grpc_client_sent_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/client/sent_bytes_per_rpc",
-                                                                  "Total bytes sent across all request messages per RPC",
-                                                                  byte)
-        grpc_client_received_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/client/received_bytes_per_rpc",
-                                                                      "Total bytes received across all response messages per RPC",
-                                                                      byte)
-        grpc_client_sent_bytes_per_method = measure_double.MeasureDouble("grpc.io/client/sent_bytes_per_method",
-                                                                         "Total bytes sent per method, recorded real-time as bytes are sent.",
-                                                                         byte)
-        grpc_client_received_bytes_per_method = measure_double.MeasureDouble("grpc.io/client/received_bytes_per_method",
-                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
-                                                                             byte)
-        grpc_client_sent_messages_per_method = measure_long.MeasureLong("grpc.io/client/sent_messages_per_method",
-                                                                        "Total messages sent per method.",
-                                                                        count)
-        grpc_client_received_messages_per_method = measure_long.MeasureLong("grpc.io/client/received_messages_per_method",
-                                                                            "Total messages received per method.",
-                                                                            count)
-        grpc_client_roundtrip_latency = measure_double.MeasureDouble("grpc.io/client/roundtrip_latency",
-                                                                     "Time between first byte of request sent to last byte of response received or terminal error.",
-                                                                     millisecond)
+        #server tags
+        # gRPC server status code returned, e.g. OK, CANCELLED, DEADLINE_EXCEEDED
+        grpc_server_status = tag_key.TagKey("grpc_server_status")
+        # Full gRPC method name, including package, service and method, e.g. com.exampleapi.v4.BookshelfService/Checkout
+        grpc_server_method = tag_key.TagKey("grpc_server_method")
+
+
+        #client measures
         grpc_client_sent_messages_per_rpc = measure_long.MeasureLong("grpc.io/client/sent_messages_per_rpc",
                                                                      "Number of messages sent in the RPC",
-                                                                     count)
+                                                                     self.count)
+        grpc_client_sent_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/client/sent_bytes_per_rpc",
+                                                                  "Total bytes sent across all request messages per RPC",
+                                                                  self.byte)
         grpc_client_received_messages_per_rpc = measure_long.MeasureLong("grpc.io/client/received_messages_per_rpc",
                                                                          "Number of response messages received per RPC",
-                                                                         count)
+                                                                         self.count)
+        grpc_client_received_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/client/received_bytes_per_rpc",
+                                                                      "Total bytes received across all response messages per RPC",
+                                                                      self.byte)
+        grpc_client_roundtrip_latency = measure_double.MeasureDouble("grpc.io/client/roundtrip_latency",
+                                                                     "Time between first byte of request sent to last byte of response received or terminal error.",
+                                                                     self.millisecond)
         grpc_client_server_latency = measure_double.MeasureDouble("grpc.io/client/server_latency",
                                                                   "Server latency in msecs",
-                                                                  millisecond)
-        grpc_client_started_rpcs = measure_long.MeasureLong( "grpc.io/client/started_rpcs",
+                                                                  self.millisecond)
+        grpc_client_started_rpcs = measure_long.MeasureLong("grpc.io/client/started_rpcs",
                                                              "Number of started client RPCs.",
-                                                             count)
+                                                             self.count)
+        grpc_client_sent_messages_per_method = measure_long.MeasureLong("grpc.io/client/sent_messages_per_method",
+                                                                        "Total messages sent per method.",
+                                                                        self.count)
+        grpc_client_received_messages_per_method = measure_long.MeasureLong("grpc.io/client/received_messages_per_method",
+                                                                            "Total messages received per method.",
+                                                                            self.count)
+        grpc_client_sent_bytes_per_method = measure_double.MeasureDouble("grpc.io/client/sent_bytes_per_method",
+                                                                         "Total bytes sent per method, recorded real-time as bytes are sent.",
+                                                                         self.byte)
+        grpc_client_received_bytes_per_method = measure_double.MeasureDouble("grpc.io/client/received_bytes_per_method",
+                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
+                                                                             self.byte)
+
+
+        # server measures
+        grpc_server_received_messages_per_rpc = measure_long.MeasureLong("grpc.io/server/received_messages_per_rpc",
+                                                                         "Number of messages received in each RPC",
+                                                                         self.count)
+        grpc_server_received_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/server/received_bytes_per_rpc",
+                                                                          "Total bytes received across all messages per RPC",
+                                                                          self.byte)
+        grpc_server_sent_messages_per_rpc = measure_long.MeasureLong("grpc.io/server/sent_messages_per_rpc",
+                                                                     "Number of messages sent in each RPC",
+                                                                     self.count)
+        grpc_server_sent_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/server/sent_bytes_per_rpc",
+                                                                      "Total bytes sent across all response messages per RPC",
+                                                                      self.byte)
+        grpc_server_server_latency = measure_double.MeasureDouble("grpc.io/server/server_latency",
+                                                                  "Time between first byte of request received to last byte of response sent or terminal error.",
+                                                                  self.millisecond)
+        grpc_server_started_rpcs = measure_long.MeasureLong("grpc.io/server/started_rpcs",
+                                                            "Number of started server RPCs.",
+                                                            self.count)
+        grpc_server_sent_messages_per_method = measure_long.MeasureLong("grpc.io/server/sent_messages_per_method",
+                                                                        "Total messages sent per method.",
+                                                                        self.count)
+        grpc_server_received_messages_per_method = measure_long.MeasureLong("grpc.io/server/received_messages_per_method",
+                                                                            "Total messages received per method.",
+                                                                            self.count)
+        grpc_server_sent_bytes_per_method  = measure_double.MeasureDouble("grpc.io/server/sent_bytes_per_method",
+                                                                          "Total bytes sent per method, recorded real-time as bytes are sent.",
+                                                                          self.byte)
+        grpc_server_received_bytes_per_method = measure_double.MeasureDouble("grpc.io/server/received_bytes_per_method",
+                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
+                                                                             self.byte)
+
+
+
+
+
+
+        """
         @deprecated(version=0.8, reason="because error counts can be computed on your metrics backend by totalling the different per-status values")
         rpc_client_error_count = measure_long.MeasureLong("grpc.io/client/error_count",
                                                           "RPC Errors",
@@ -96,34 +143,8 @@ class RPCMeasureConstants():
 
         @deprecated()
         rpc_client_response_count = grpc_client_received_messages_per_rpc
-        grpc_server_sent_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/server/sent_bytes_per_rpc",
-                                                                      "Total bytes sent across all response messages per RPC",
-                                                                      byte)
-        grpc_server_received_bytes_per_rpc = measure_double.MeasureDouble("grpc.io/server/received_bytes_per_rpc",
-                                                                          "Total bytes received across all messages per RPC",
-                                                                          byte)
-        grpc_server_sent_bytes_per_method  = measure_double.MeasureDouble("grpc.io/server/sent_bytes_per_method",
-                                                                          "Total bytes sent per method, recorded real-time as bytes are sent.",
-                                                                          byte)
-        grpc_server_received_bytes_per_method = measure_double.MeasureDouble("grpc.io/server/received_bytes_per_method",
-                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
-                                                                             byte)
-        grpc_server_sent_messages_per_method = measure_long.MeasureLong("grpc.io/server/sent_messages_per_method",
-                                                                        "Total messages sent per method.",
-                                                                        count)
-        grpc_server_received_messages_per_method = measure_long.MeasureLong("grpc.io/server/received_messages_per_method",
-                                                                            "Total messages received per method.",
-                                                                            count)
-        grpc_server_sent_messages_per_rpc = measure_long.MeasureLong("grpc.io/server/sent_messages_per_rpc", "Number of messages sent in each RPC", count)
-        grpc_server_received_messages_per_rpc = measure_long.MeasureLong("grpc.io/server/received_messages_per_rpc",
-                                                                         "Number of messages received in each RPC",
-                                                                         count)
-        grpc_server_server_latency = measure_double.MeasureDouble("grpc.io/server/server_latency",
-                                                                  "Time between first byte of request received to last byte of response sent or terminal error.",
-                                                                  millisecond)
-        grpc_server_started_rpcs = measure_long.MeasureLong("grpc.io/server/started_rpcs",
-                                                            "Number of started server RPCs.",
-                                                            count)
+
+
         rpc_server_error_count = measure_long.MeasureLong("grpc.io/server/error_count",
                                                           "RPC Errors",
                                                           count)
@@ -144,6 +165,7 @@ class RPCMeasureConstants():
                                                               count)
         rpc_server_request_count = grpc_server_received_messages_per_rpc
         rpc_server_response_count = grpc_server_sent_messages_per_rpc
+        """
 
 
 
