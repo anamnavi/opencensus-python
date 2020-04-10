6eddcf577249773a09eb49d0ba369e5bb1b9718e
Anam Navied <anavied@ncsu.edu>
2020-04-10 00:50:45 -0400

    pylint has long line and too many inst attrib errors

--- opencensus/trace/rpc_measure_constants.py
+++ opencensus/trace/rpc_measure_constants.py
@@ -1,13 +1,11 @@
 from opencensus.tags import tag_key
-# from opencensus.trace import measure_double, measure_long
-# import measure_long
-# import measure_double
 import measure
 
 
 class RPCMeasureConstants():
     """
     constants for collecting RPC stats
+    see specs in documentation for opencensus-python
     """
     byte = "by"
     count = "1"
@@ -19,83 +17,82 @@ class RPCMeasureConstants():
         todo: do I put all the RPC constant fields in here?
         """
 
-        #client tags
+        # client tags
         # gRPC server status code received, e.g. OK, CANCELLED, DEADLINE_EXCEEDED
         self.grpc_client_status = tag_key.TagKey("grpc_client_status")
         # Full gRPC method name, including package, service and method, e.g. google.bigtable.v2.Bigtable/CheckAndMutateRow
         self.grpc_client_method = tag_key.TagKey("grpc_client_method")
 
-        #server tags
+        # server tags
         # gRPC server status code returned, e.g. OK, CANCELLED, DEADLINE_EXCEEDED
         self.grpc_server_status = tag_key.TagKey("grpc_server_status")
         # Full gRPC method name, including package, service and method, e.g. com.exampleapi.v4.BookshelfService/Checkout
         self.grpc_server_method = tag_key.TagKey("grpc_server_method")
 
 
-        #client measures
+        # client measures
         self.grpc_client_sent_messages_per_rpc = measure.Measure("grpc.io/client/sent_messages_per_rpc",
-                                                                     "Number of messages sent in the RPC",
-                                                                     self.count)
+                                                                 "Number of messages sent in the RPC",
+                                                                 self.count)
         self.grpc_client_sent_bytes_per_rpc = measure.Measure("grpc.io/client/sent_bytes_per_rpc",
-                                                                  "Total bytes sent across all request messages per RPC",
-                                                                      self.byte)
+                                                              "Total bytes sent across all request messages per RPC",
+                                                              self.byte)
         self.grpc_client_received_messages_per_rpc = measure.Measure("grpc.io/client/received_messages_per_rpc",
-                                                                         "Number of response messages received per RPC",
-                                                                         self.count)
+                                                                     "Number of response messages received per RPC",
+                                                                     self.count)
         self.grpc_client_received_bytes_per_rpc = measure.Measure("grpc.io/client/received_bytes_per_rpc",
-                                                                      "Total bytes received across all response messages per RPC",
-                                                                          self.byte)
+                                                                  "Total bytes received across all response messages per RPC",
+                                                                  self.byte)
         self.grpc_client_roundtrip_latency = measure.Measure("grpc.io/client/roundtrip_latency",
-                                                                     "Time between first byte of request sent to last byte of response received or terminal error.",
-                                                                     self.millisecond)
+                                                             "Time between first byte of request sent to last byte of response received or terminal error.",
+                                                             self.millisecond)
         self.grpc_client_server_latency = measure.Measure("grpc.io/client/server_latency",
-                                                                  "Server latency in msecs",
-                                                                  self.millisecond)
+                                                          "Server latency in msecs",
+                                                          self.millisecond)
         self.grpc_client_started_rpcs = measure.Measure("grpc.io/client/started_rpcs",
-                                                             "Number of started client RPCs.",
-                                                            self.count)
+                                                        "Number of started client RPCs.",
+                                                        self.count)
         self.grpc_client_sent_messages_per_method = measure.Measure("grpc.io/client/sent_messages_per_method",
-                                                                        "Total messages sent per method.",
-                                                                        self.count)
+                                                                    "Total messages sent per method.",
+                                                                    self.count)
         self.grpc_client_received_messages_per_method = measure.Measure("grpc.io/client/received_messages_per_method",
-                                                                            "Total messages received per method.",
-                                                                            self.count)
+                                                                        "Total messages received per method.",
+                                                                        self.count)
         self.grpc_client_sent_bytes_per_method = measure.Measure("grpc.io/client/sent_bytes_per_method",
-                                                                         "Total bytes sent per method, recorded real-time as bytes are sent.",
-                                                                         self.byte)
+                                                                 "Total bytes sent per method, recorded real-time as bytes are sent.",
+                                                                 self.byte)
         self.grpc_client_received_bytes_per_method = measure.Measure("grpc.io/client/received_bytes_per_method",
-                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
-                                                                             self.byte)
-
+                                                                     "Total bytes received per method, recorded real-time as bytes are received.",
+                                                                     self.byte)
 
         # server measures
         self.grpc_server_received_messages_per_rpc = measure.Measure("grpc.io/server/received_messages_per_rpc",
-                                                                         "Number of messages received in each RPC",
-                                                                         self.count)
-        self.grpc_server_received_bytes_per_rpc = measure.Measure("grpc.io/server/received_bytes_per_rpc",
-                                                                          "Total bytes received across all messages per RPC",
-                                                                          self.byte)
-        self.grpc_server_sent_messages_per_rpc = measure.Measure("grpc.io/server/sent_messages_per_rpc",
-                                                                     "Number of messages sent in each RPC",
+                                                                     "Number of messages received in each RPC",
                                                                      self.count)
+        self.grpc_server_received_bytes_per_rpc = measure.Measure("grpc.io/server/received_bytes_per_rpc",
+                                                                  "Total bytes received across all messages per RPC",
+                                                                  self.byte)
+        self.grpc_server_sent_messages_per_rpc = measure.Measure("grpc.io/server/sent_messages_per_rpc",
+                                                                 "Number of messages sent in each RPC",
+                                                                 self.count)
         self.grpc_server_sent_bytes_per_rpc = measure.Measure("grpc.io/server/sent_bytes_per_rpc",
-                                                                      "Total bytes sent across all response messages per RPC",
-                                                                      self.byte)
+                                                              "Total bytes sent across all response messages per RPC",
+                                                              self.byte)
         self.grpc_server_server_latency = measure.Measure("grpc.io/server/server_latency",
-                                                                  "Time between first byte of request received to last byte of response sent or terminal error.",
-                                                                  self.millisecond)
+                                                          "Time between first byte of request received to last byte of response sent or terminal error.",
+                                                          self.millisecond)
         self.grpc_server_started_rpcs = measure.Measure("grpc.io/server/started_rpcs",
-                                                            "Number of started server RPCs.",
-                                                            self.count)
+                                                        "Number of started server RPCs.",
+                                                        self.count)
         self.grpc_server_sent_messages_per_method = measure.Measure("grpc.io/server/sent_messages_per_method",
-                                                                        "Total messages sent per method.",
-                                                                        self.count)
+                                                                    "Total messages sent per method.",
+                                                                    self.count)
         self.grpc_server_received_messages_per_method = measure.Measure("grpc.io/server/received_messages_per_method",
-                                                                            "Total messages received per method.",
-                                                                            self.count)
-        self.grpc_server_sent_bytes_per_method  = measure.Measure("grpc.io/server/sent_bytes_per_method",
-                                                                          "Total bytes sent per method, recorded real-time as bytes are sent.",
-                                                                          self.byte)
+                                                                        "Total messages received per method.",
+                                                                        self.count)
+        self.grpc_server_sent_bytes_per_method = measure.Measure("grpc.io/server/sent_bytes_per_method",
+                                                                 "Total bytes sent per method, recorded real-time as bytes are sent.",
+                                                                 self.byte)
         self.grpc_server_received_bytes_per_method = measure.Measure("grpc.io/server/received_bytes_per_method",
-                                                                             "Total bytes received per method, recorded real-time as bytes are received.",
-                                                                             self.byte)
+                                                                     "Total bytes received per method, recorded real-time as bytes are received.",
+                                                                     self.byte)
