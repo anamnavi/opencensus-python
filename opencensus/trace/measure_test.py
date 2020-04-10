eb99f84a8a0571f14cdfc0bb745ae7df24902acb
Anam Navied <anavied@ncsu.edu>
2020-04-10 00:15:59 -0400

    pylint for measure_test.py

--- opencensus/trace/measure_test.py
+++ opencensus/trace/measure_test.py
@@ -2,24 +2,55 @@ import unittest
 import measure
 
 class MeasureTest(unittest.TestCase):
+    """
+    MeasureTest has PyUnit tests for measure.py
+    """
 
     def setUp(self):
-        self.measure = measure.Measure("grpc_client_sent_messages_per_rpc", "Number of messages sent in the RPC", "1")
+        """
+        setup performed before each test
+        """
+        self.measure = measure.Measure("grpc_client_sent_messages_per_rpc",
+                                       "Number of messages sent in the RPC",
+                                       "1")
 
     def test_measure_name(self):
-        self.assertEqual(self.measure.get_name(), "grpc_client_sent_messages_per_rpc", "get_name() method not working")
+        """
+        tests measure module's get_name() method
+        """
+        self.assertEqual(self.measure.get_name(),
+                         "grpc_client_sent_messages_per_rpc",
+                         "get_name() method not working")
 
     def test_measure_description(self):
-        self.assertEqual(self.measure.get_description(), "Number of messages sent in the RPC", "get_description() method not working")
+        """
+        tests measure module's get_description() method
+        """
+        self.assertEqual(self.measure.get_description(),
+                         "Number of messages sent in the RPC",
+                         "get_description() method not working")
 
     def test_measure_unit(self):
-        self.assertEqual(self.measure.get_unit(), "1", "get_unit() not working correctly")
+        """
+        tests measure module's get_unit() method
+        """
+        self.assertEqual(self.measure.get_unit(),
+                         "1",
+                         "get_unit() not working correctly")
 
     def test_long_name_init(self):
+        """
+        tests measure module's __init__() if the name str exceeds max length
+        and assert that it raises ValueError as expected
+        """
         with self.assertRaises(ValueError):
             self.measure_exceeds_max_length = measure.Measure("a"*256, "some description", "1")
 
     def test_unprintable_name_init(self):
+        """
+        tests measure module's __init__() if the name str contains a non-printable ascii character
+        and assert that it raises ValueError as expected
+        """
         with self.assertRaises(ValueError):
             chr_list = ['0x41', '0x1B', '0x42']
             non_printable_str = "".join([chr(int(x, 16)) for x in chr_list])  # 'AB\x1b'
