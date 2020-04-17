2b5738ce739e6e28dbe0013f34a9fa97efb58d6a
Anam Navied <anavied@ncsu.edu>
2020-04-16 20:19:00 -0400

    added tests for client views

--- opencensus/trace/rpc_v_c_test.py
+++ opencensus/trace/rpc_v_c_test.py
@@ -1,5 +1,6 @@
 import unittest
 import rpc_view_constants
+from opencensus.metrics.export.metric_descriptor import MetricDescriptorType
 
 class RPCVCTests(unittest.TestCase):
     """
@@ -10,7 +11,44 @@ class RPCVCTests(unittest.TestCase):
         self.rpc_view = rpc_view_constants.RPCViewConstants()
 
     def test_client_measures(self):
-        self.assertEquals(self.rpc_view.grpc_client_sent_bytes_per_rpc_view.get_metric_descriptor().name, "grpc.io/client/sent_bytes_per_rpc", "view not set right")
+        self.assertEqual(self.rpc_view.grpc_client_sent_bytes_per_rpc_view.name,
+                          "grpc.io/client/sent_bytes_per_rpc",
+                          "grpc_client_sent_bytes_per_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_received_bytes_per_rpc_view.description,
+                         "Received bytes per RPC",
+                         "grpc_client_received_bytes_per_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_roundtrip_latency_view.measure.get_name(),
+                         "grpc.io/client/roundtrip_latency",
+                         "grpc_client_roundtrip_latency_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_completed_rpc_view.measure.get_name(),
+                         "grpc.io/client/roundtrip_latency",
+                         "grpc_client_completed_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_started_rpc_view.description,
+                         "Number of started client RPCs",
+                         "grpc_client_started_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_sent_messages_per_rpc_view.name,
+                         "grpc.io/client/sent_messages_per_rpc",
+                         "grpc_client_sent_messages_per_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_received_messages_per_rpc_view.description,
+                         "Number of response messages received per RPC",
+                         "grpc_client_received_messages_per_rpc_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_server_latency_view.measure.get_unit(),
+                         "ms",
+                         "grpc_client_server_latency_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_sent_messages_per_method_view.name,
+                         "grpc.io/client/sent_messages_per_method",
+                         "grpc_client_sent_messages_per_method_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_received_messages_per_method_view.description,
+                         "Number of messages received",
+                         "grpc_client_received_messages_per_method_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_sent_bytes_per_method_view.measure.get_unit(),
+                         "by",
+                         "grpc_client_sent_bytes_per_method_view not set correctly")
+        self.assertEqual(self.rpc_view.grpc_client_received_bytes_per_method_view.name,
+                         "grpc.io/client/received_bytes_per_method",
+                         "grpc_client_received_bytes_per_method_view not set correctly")
+
+
 
 
 if __name__ == '__main__':
